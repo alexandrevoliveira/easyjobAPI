2e1cee05e4cdb86f40aa6392ad7e2127673a53dc
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-08-18 09:37:39 -0300

    evitando cadastro caso ja exista determinado email ou cnpj

--- src/modules/companies/repositories/ICompaniesRepository.ts
+++ src/modules/companies/repositories/ICompaniesRepository.ts
@@ -4,6 +4,7 @@ import { Company } from "../infra/typeorm/entities/Company";
 interface ICompaniesRepository {
   create({ name, email, password, cnpj }: ICreateCompanyDTO): Promise<void>;
   findByEmail(email: string): Promise<Company>;
+  findByEmailOrCnpj(email: string, cnpj: string): Promise<Company>;
   list(): Promise<Company[]>;
 }
 
