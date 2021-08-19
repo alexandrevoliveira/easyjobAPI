16be35d18187d32c0afe82466e0b582c1b81050e
Danilo William <dwpl@cin.ufpe.br>
2021-08-18 23:34:40 -0300

    feat: adicao da entidade de Candidato

---
+++ src/modules/candidates/infra/typeorm/entities/Candidate.ts
@@ -0,0 +1,43 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity("candidates")
+class Candidate {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  cpf: string;
+
+  @Column("simple-array")
+  skills?: string[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Candidate };
