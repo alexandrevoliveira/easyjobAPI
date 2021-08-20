26d25fc5c33731f378a69d6b9cb0696bbf939cab
Dario Gabriel <dgsv@cin.ufpe.br>
2021-08-20 14:57:23 -0300

    feat: adicao do controller de listagem de vagas

--- src/shared/infra/http/routes/vacancies.routes.ts
+++ src/shared/infra/http/routes/vacancies.routes.ts
@@ -1,10 +1,13 @@
 import { CreateVacancyController } from "@modules/vacancies/useCases/createVacancy/CreateVacancyController";
+import { ListVacanciesController } from "@modules/vacancies/useCases/listVacancies/ListVacanciesController";
 import { Router } from "express";
 
 const vacanciesRoutes = Router();
 
 const createVacancyController = new CreateVacancyController();
+const listVacanciesController = new ListVacanciesController();
 
 vacanciesRoutes.post("/", createVacancyController.handle);
+vacanciesRoutes.get("/", listVacanciesController.handle);
 
 export { vacanciesRoutes };
