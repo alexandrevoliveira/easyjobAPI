eb1cd4f39a4254e1336a5889a595cf7e24c22d25
Dario Gabriel <dgsv@cin.ufpe.br>
2021-08-20 14:56:44 -0300

    feat: adicao do useCase de listagem de vagas

---
+++ src/modules/vacancies/useCases/listVacancies/ListVacanciesUseCase.ts
@@ -0,0 +1,67 @@
+import { inject, injectable } from "tsyringe";
+
+import { IVacanciesRepository } from "../../repositories/IVacanciesRepository";
+
+interface IRequest {
+  page: any;
+  per_page: any;
+}
+
+type Vacancy = {
+  role: string;
+  type: string;
+  area: string;
+  requirements: string[];
+  salary: number;
+  quantity: number;
+  company_id: string;
+};
+
+interface IResponse {
+  vacancies: Vacancy[];
+  total: number;
+}
+
+@injectable()
+class ListVacanciesUseCase {
+  constructor(
+    @inject("VacanciesRepository")
+    private vacanciesRepository: IVacanciesRepository
+  ) {}
+
+  async execute({ page = 1, per_page = 10 }: IRequest): Promise<IResponse> {
+    let vacancies = await this.vacanciesRepository.list();
+
+    const total = vacancies.length;
+
+    const pageStart = (Number(page) - 1) * Number(per_page);
+    const pageEnd = pageStart + Number(per_page);
+
+    vacancies = vacancies.slice(pageStart, pageEnd);
+
+    const vacanciesFormatted: Vacancy[] = vacancies.map((vacancy) => {
+      const vacancyFormatted = {
+        id: vacancy.id,
+        role: vacancy.role,
+        type: vacancy.type,
+        area: vacancy.area,
+        requirements: vacancy.requirements,
+        salary: vacancy.salary,
+        quantity: vacancy.quantity,
+        company_id: vacancy.company_id,
+        created_at: vacancy.created_at,
+        updated_at: vacancy.updated_at,
+      };
+      return vacancyFormatted;
+    });
+
+    const listVacanciesUseCaseResponse: IResponse = {
+      vacancies: vacanciesFormatted,
+      total,
+    };
+
+    return listVacanciesUseCaseResponse;
+  }
+}
+
+export { ListVacanciesUseCase };
