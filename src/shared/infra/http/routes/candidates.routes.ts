c26249757072e146508089aa9801f0cd4a2c724c
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-08-23 19:24:47 -0300

    feat: adicao da rota de autenticacao do candidato

--- src/shared/infra/http/routes/candidates.routes.ts
+++ src/shared/infra/http/routes/candidates.routes.ts
@@ -1,3 +1,4 @@
+import { AuthenticateCandidateController } from "@modules/candidates/useCases/authenticateCandidate/AuthenticateCandidateController";
 import { CreateCandidateController } from "@modules/candidates/useCases/createCandidate/CreateCandidateController";
 import { ListCandidatesController } from "@modules/candidates/useCases/listCandidates/ListCandidatesController";
 import { Router } from "express";
@@ -6,8 +7,12 @@ const candidatesRoutes = Router();
 
 const createCandidateController = new CreateCandidateController();
 const listCandidatesController = new ListCandidatesController();
+const authenticateCandidateController = new AuthenticateCandidateController();
 
 candidatesRoutes.post("/", createCandidateController.handle);
 candidatesRoutes.get("/", listCandidatesController.handle);
+
+// sessions
+candidatesRoutes.post("/sessions", authenticateCandidateController.handle);
 
 export { candidatesRoutes };
