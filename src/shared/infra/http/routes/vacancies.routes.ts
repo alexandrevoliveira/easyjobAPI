717df47b2288de1b92879c9f31db76bf8195b418
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-08-23 20:17:18 -0300

    feat: adicao da rota de visualizacao de vaga pelo id

--- src/shared/infra/http/routes/vacancies.routes.ts
+++ src/shared/infra/http/routes/vacancies.routes.ts
@@ -1,13 +1,16 @@
 import { CreateVacancyController } from "@modules/vacancies/useCases/createVacancy/CreateVacancyController";
 import { ListVacanciesController } from "@modules/vacancies/useCases/listVacancies/ListVacanciesController";
+import { ShowVacancyController } from "@modules/vacancies/useCases/showVacancy/ShowVacancyController";
 import { Router } from "express";
 
 const vacanciesRoutes = Router();
 
 const createVacancyController = new CreateVacancyController();
 const listVacanciesController = new ListVacanciesController();
+const showVacancyController = new ShowVacancyController();
 
 vacanciesRoutes.post("/", createVacancyController.handle);
 vacanciesRoutes.get("/", listVacanciesController.handle);
+vacanciesRoutes.get("/:id", showVacancyController.handle);
 
 export { vacanciesRoutes };
