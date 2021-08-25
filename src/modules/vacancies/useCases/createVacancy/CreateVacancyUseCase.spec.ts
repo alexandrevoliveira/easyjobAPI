f6e3c14c9554160431b89f8c1efb67a7cf8612be
Dario Gabriel <dgsv@cin.ufpe.br>
2021-08-25 08:02:25 -0300

    test: mais testes de criacao de vaga

--- src/modules/vacancies/useCases/createVacancy/CreateVacancyUseCase.spec.ts
+++ src/modules/vacancies/useCases/createVacancy/CreateVacancyUseCase.spec.ts
@@ -1,5 +1,7 @@
 import { VacanciesRepositoryInMemory } from "@modules/vacancies/repositories/in-memory/VacanciesRepositoryInMemory";
 
+import { AppError } from "@shared/errors/AppError";
+
 import { CreateVacancyUseCase } from "./CreateVacancyUseCase";
 
 let createVacancyUseCase: CreateVacancyUseCase;
@@ -25,5 +27,29 @@ describe("Create Vacancy", () => {
     });
 
     expect(vacancy).toHaveProperty("id");
+  });
+
+  it("should not be able to create a vacancy with existing role", async () => {
+    await createVacancyUseCase.execute({
+      role: "Role",
+      type: "Type",
+      area: "IT",
+      requirements: ["requirement"],
+      salary: 16000,
+      quantity: 5,
+      company_id: "company",
+    });
+
+    expect(async () => {
+      await createVacancyUseCase.execute({
+        role: "Role",
+        type: "Type CLT",
+        area: "Design",
+        requirements: ["requirement"],
+        salary: 8000,
+        quantity: 3,
+        company_id: "company",
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
