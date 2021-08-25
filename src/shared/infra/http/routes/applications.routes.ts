518ec181100abda1a032ab99194a13b459535a59
Dario Gabriel <dgsv@cin.ufpe.br>
2021-08-25 00:13:58 -0300

    feat: adicao da rota de listagem de candidaturas

--- src/shared/infra/http/routes/applications.routes.ts
+++ src/shared/infra/http/routes/applications.routes.ts
@@ -1,10 +1,13 @@
 import { CreateApplicationController } from "@modules/applications/useCases/createApplication/CreateApplicationController";
+import { ListApplicationsController } from "@modules/applications/useCases/listApplications/ListApplicationsController";
 import { Router } from "express";
 
 const applicationsRoutes = Router();
 
 const createApplicationController = new CreateApplicationController();
+const listApplicationsController = new ListApplicationsController();
 
 applicationsRoutes.post("/", createApplicationController.handle);
+applicationsRoutes.get("/", listApplicationsController.handle);
 
 export { applicationsRoutes };
